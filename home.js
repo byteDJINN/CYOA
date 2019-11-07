2163d9a7bbfc84175fc011acb75847fdc01cb4ea
Djimon Jayasundera <1015097@student.ccgs.wa.edu.au>
2019-11-07 10:08:13 +0800

    enhanced home ui

--- home.js
+++ home.js
@@ -49,13 +49,8 @@ function check_cookie(x, backup) {
 
     }
 }
-
-function get_adventure() {
-    var file = prompt("Enter Filename: ", "journey_under_the_sea.txt");
-    if (file != null) {
-        set_cookie("file", file);
-        set_cookie("id", "start");
-        window.location.href = "cyoa.html";
-    }
-
+function select_cyoa(filename) {
+    set_cookie("file", filename);
+    set_cookie("id", 'start');
+    window.location.href = "cyoa.html";
 }
